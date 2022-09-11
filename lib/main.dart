8643fee60aad9e351b240f3581b0faee91e3fa99
Majid Pourbarzegar <pbr.programmer@gmail.com>
2022-09-11 10:55:22 +0430

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:ffi';
 import 'package:flutter/material.dart';
 
 void main() {
+<<<<<<< HEAD
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     // title: AppBar(),
@@ -16,4 +17,29 @@ void main() {
           title: const Text('Majid')),
     ),
   ));
+=======
+  var application = Aplication();
+  runApp(application);
+}
+
+class Aplication extends StatefulWidget {
+  Aplication({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _ApplicationState();
+}
+
+class _ApplicationState extends State<Aplication> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          body: SafeArea(
+            child: Text("Majid Pourbarzegar"),
+          )),
+    );
+  }
+>>>>>>> 1-is-the-first-issues
 }
